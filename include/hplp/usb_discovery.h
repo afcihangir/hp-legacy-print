f9b37aa8d8b27cdb94c97a74c15fe4efbc5170e8
afcihangir <afcihangir@gmail.com>
2026-09-23 14:39:51 +0300

    Track USB serial readiness explicitly

--- include/hplp/usb_discovery.h
+++ include/hplp/usb_discovery.h
@@ -6,12 +6,20 @@
 
 #include "hplp/models.h"
 
+typedef enum {
+    HPLP_SERIAL_ABSENT = 0,
+    HPLP_SERIAL_READY = 1,
+    HPLP_SERIAL_PENDING = 2
+} hplp_serial_state_t;
+
 typedef struct {
     uint8_t bus_number;
     uint8_t device_address;
     uint16_t vendor_id;
     uint16_t product_id;
     char serial[256];
+    hplp_serial_state_t serial_state;
+    int serial_error;
     const hplp_model_t *model;
 } hplp_usb_device_t;
 
