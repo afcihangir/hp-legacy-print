2b3367f0ccaa0d3a096467e9a1ffe71049f1deff
afcihangir <afcihangir@gmail.com>
2026-09-23 15:21:52 +0300

    Expose reusable P1102 render helper

--- include/hplp/render_test.h
+++ include/hplp/render_test.h
@@ -1,6 +1,9 @@
 #ifndef HPLP_RENDER_TEST_H
 #define HPLP_RENDER_TEST_H
 
+#include <stddef.h>
+
 int hplp_render_p1102_test(void);
+int hplp_render_p1102_test_file(char *path, size_t path_size, long long *bytes_out);
 
 #endif
